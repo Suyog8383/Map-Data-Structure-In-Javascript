b91a61e0a989c7a3fb2574f6e850ed245cc7a6e8
Suyog Kalyan Nagawade <92072200+Suyog8383@users.noreply.github.com>
2023-07-04 21:29:58 +0530

    Create map.js

---
+++ map.js
@@ -0,0 +1,17 @@
+// Obj
+// let obj={
+//     name:"suyog"    //key=> string
+// }
+//Map
+// let map={
+//     name => "suyog" //key => any datatype
+// }
+let data=new Map([
+    ["name","suyog"],
+    [true,"nagawade"]
+    ])
+    
+data.set("age",23)
+console.log(data.size);
+console.log(data);
+console.log(data.has("age"))
